fdd0ea0ad3810937600c6df4b8673b3f19256b63
Henrik Kastrup Witt <henrikkastrup@gmail.com>
2018-06-14 22:21:41 +0200

    changes 14.06.2018

--- sketch.js
+++ sketch.js
@@ -1,18 +1,8 @@
 // Main file in the prisoners dilemma template
 
+let scorematrix = [[-2, 0], [-1, 3]]; // Score matrix of the prisoners dilemma
 
-
-
-
-
-
-function setup() {
-    let player1 = new player("Henrik", 0);
-}
-
-function draw() {
-    noLoop();
-}
+let rounds = 10;
 
 class player {
     constructor(name, strategi) {
@@ -21,5 +11,39 @@ class player {
         this.strategi = strategi;
     }
     
+    answer(){
+        return this.strategi;
+    }
+}
+
+let player1 = new player("Henrik", 0);
+let player2 = new player("Computer", 1);
+
+
+
+
+function setup() {
+    createCanvas(200,200);
     
+}
+
+function mousePressed() {
+    game(player1, player2);
+    
+}
+
+function draw() {
+    for(var i = 0; i < rounds; i++) {
+    game(player1, player2);
+  }
+    console.log(playerA.score);
+    console.log(playerB.score);
+    noLoop();
+}
+
+
+
+function game(playerA, playerB) {
+    playerA.score += scorematrix[playerA.answer()][playerB.answer()];
+    playerB.score += scorematrix[playerB.answer()][playerA.answer()];
 }
