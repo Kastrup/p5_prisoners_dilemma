c41dcd05db708114cf00e8e0fd59366a9bab67ff
Henrik Kastrup Witt <henrikkastrup@gmail.com>
2018-06-14 21:35:18 +0200

    changes to sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,25 @@
 // Main file in the prisoners dilemma template
 
-function setup() {
 
+
+
+
+
+
+function setup() {
+    let player1 = new player("Henrik", 0);
 }
 
 function draw() {
+    noLoop();
+}
 
+class player {
+    constructor(name, strategi) {
+        this.name = name;
+        this.score = 0;
+        this.strategi = strategi;
+    }
+    
+    
 }
