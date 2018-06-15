c556cd1a59c6b540157e85a60d376f82bfd64051
henrikkastrup <hkj@naviair.dk>
2018-06-15 09:50:48 +0200

    Tit for tat and Tit for tat last defect works

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 // Main file in the prisoners dilemma template
 
-let scorematrix = [[-1, 0], [-3, -2]]; // Score matrix of the prisoners dilemma
+let scorematrix = [[3, 1], [4, 2]]; // Score matrix of the prisoners dilemma
 let rounds = 100; // how many rounds in each game
 players = []; // Array to keep the players
 num_players = 4 // Number of players
@@ -16,7 +16,7 @@ class player {
         
     }
         
-    answer(){
+    answer(last){
     	var move;
     	if (this.strategi == 0){ // Allways answer 0
     		move = 0; }
@@ -28,8 +28,24 @@ class player {
      		var x = int(random(0, 11));
      		if (x < 10) { move = 0; }
      			else {move = 1; }
-
     		}
+    		
+    	else if (this.strategi == 4) { // Tit for Tat
+    		move = 0;
+    		if (this.countermoves.length > 1) {
+    			move = this.countermoves[this.countermoves.length - 2];
+    		}
+		}
+   			
+    		
+    	else if (this.strategi == 5) { // Tit for Tat with last defect
+       		move = 0;
+    		if (this.countermoves.length > 1) {
+    			move = this.countermoves[this.countermoves.length - 2];
+    		}
+		if (last) { move = 1; }
+    		}	
+    		
     	this.moves.push(move);
     	return move;	
     	
@@ -39,11 +55,13 @@ class player {
 
 function setup() {
     createCanvas(200,200);
-    players.push(new player("Henrik", 0));
-    players.push(new player("Computer", 1));
-    players.push(new player("Com2", 2));
-    players.push(new player("Com3", 3));}
-
+    players.push(new player("Allways_Cooperate", 0));
+    players.push(new player("Allways_Reject", 1));
+    players.push(new player("Random_50_50", 2));
+    players.push(new player("Random_70_30", 3));
+    players.push(new player("Tit4Tat",4));
+    players.push(new player("Tit4Tat_defect_last",5));
+  }
 
 
 
@@ -51,10 +69,19 @@ function draw() {
     
     for(var i = 0; i < players.length; i++) {
     	for(var j = i; j < players.length; j++) {
+	    	players[i].moves = [];
+    		players[i].countermoves = [];
+		players[j].moves = [];
+    		players[j].countermoves = [];
+    	    		
     		for(var k = 0; k < rounds; k++) {
-    	    		if (i != j) {
-    	    			game(players[i], players[j]);
-    	    			console.log(players[i].name + " " +  players[j].name); 
+    			var last = 'false';
+    			if (i != j) {
+    				if (k + 1 == rounds) { 
+    					last = 'true'; 
+    					}
+    	    			game(players[i], players[j], last);
+    	    			//console.log(players[i].name + " " +  players[j].name); 
     	    			}
     	    	}
     	    }
@@ -67,10 +94,10 @@ function draw() {
 
 
 
-function game(playerA, playerB) {
-    a = playerA.answer();
+function game(playerA, playerB, last) {
+    a = playerA.answer(last);
     playerB.countermoves.push(a);
-    b = playerB.answer();
+    b = playerB.answer(last);
     playerA.countermoves.push(b);
     playerA.score += scorematrix[a][b];
     playerB.score += scorematrix[b][a];
